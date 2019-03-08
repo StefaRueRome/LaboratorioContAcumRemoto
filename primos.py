efa90a1b67ac51cfb4c9ec576e04bf917fd08162
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2019-03-08 13:24:29 -0500

    Ya casi calculo un primo

--- primos.py
+++ primos.py
@@ -1,7 +1,10 @@
-print("Algoritmo que determine si un número es divisible exacto entre otro número")
-a=int(input("Ingresa el primer número: "))
-b=int(input("Ingresa el segundo número: "))
-if a%b==0:
-	print("El número:",a, "es divisible exacto entre:",b,)
-else:
-	print("El número:",a, "no es divisible exacto entre:",b,)
+resid=0
+
+a=int(input("ingrese un número: "))
+for i in range(1,a):
+	resid=a%i
+	print(resid)
+		
+
+
+
