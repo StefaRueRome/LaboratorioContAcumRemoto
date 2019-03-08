b84d1fce0a8dff3c8ded9e19136f219c15935c49
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2019-03-08 13:46:31 -0500

    Ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,10 +1,14 @@
-resid=0
+cont=0
 
 a=int(input("ingrese un número: "))
-for i in range(1,a):
-	resid=a%i
-	print(resid)
-		
+for i in range(1,a+1,1):
+	if a%i==0:
+		cont=cont+1
+	
+if cont>2:
+	print("no es número primo")
+else:
+	print("es un número primo")
 
 
 
